48641db9338f0b245e6b9b5da26e0f5c47b87222
[user_name] <cotaro.50tal@gmail.com>
2019-04-24 23:51:46 +0900

    add posts#*

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "posts/:start_time/new" => "posts#new"
+  resources :posts
   get '/' => "home#top"
   get "about" => "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
