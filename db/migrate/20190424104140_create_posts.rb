bbfe48256756a0863b758ebae0ad1b372830dc55
[user_name] <cotaro.50tal@gmail.com>
2019-05-05 14:09:31 +0900

    update posts#*

--- db/migrate/20190424104140_create_posts.rb
+++ db/migrate/20190424104140_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :content,  default: "未定"
+      t.string :content
       t.date :start_time 
 
       t.timestamps
