39dfbfa9ce833f10ea8b38cc87dd2d796c6fd610
[user_name] <cotaro.50tal@gmail.com>
2019-05-13 23:20:17 +0900

    update posts#linebot

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -5,8 +5,8 @@ class LinebotController < ApplicationController
             text: 'hello'
         }
         client = Line::Bot::Client.new { |config|
-            config.channel_secret = "<channel secret>"
-            config.channel_token = "<channel access token>"
+            config.channel_secret = "<da5be14c010d092c6a188bf9fb79f071>"
+            config.channel_token = "<l6MsxS40JGaFsZlSSR3br5fZ1i6Ofks3hELPEne65kqzM695FyfjS1fgVUMWB93Vt/XCa0mY9nZTdOJ7/eqYoQnFCzEEBDrV7kiILjIqqf7+1Nqj7vpxnmZO6vUgwYWhU4RzTt6hH49bykuVByprcwdB04t89/1O/w1cDnyilFU=>"
         }
         response = client.push_message("<to>", message)
         p response
