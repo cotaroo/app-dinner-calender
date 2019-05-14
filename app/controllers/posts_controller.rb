114131ad55a4a25c35cbcbba7f80b014f0c5e9a5
[user_name] <cotaro.50tal@gmail.com>
2019-05-14 12:40:52 +0900

    update posts#create linebot

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,6 +44,24 @@ class PostsController < ApplicationController
       if $save_date < @post.start_time
         $save_date = @post.start_time
       end
+
+      # linebot
+
+      message = @post.content
+    #   {
+    #     type: 'text',
+    #     text: 'hello'
+    # }
+    client = Line::Bot::Client.new { |config|
+        config.channel_secret = "da5be14c010d092c6a188bf9fb79f071"
+        config.channel_token = "l6MsxS40JGaFsZlSSR3br5fZ1i6Ofks3hELPEne65kqzM695FyfjS1fgVUMWB93Vt/XCa0mY9nZTdOJ7/eqYoQnFCzEEBDrV7kiILjIqqf7+1Nqj7vpxnmZO6vUgwYWhU4RzTt6hH49bykuVByprcwdB04t89/1O/w1cDnyilFU="
+    }
+    response = client.push_message("Ubb563e765d94830aa20f3a1a251de66c", message)
+    p response
+
+    # ここまで
+
+
     else
       @post = Post.where(post_params).first
       redirect_to edit_post_path(@post,start_time: @post.start_time) and return
