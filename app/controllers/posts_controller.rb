5cf504fb6e08012fd49b6915695fc910404312cc
[user_name] <cotaro.50tal@gmail.com>
2019-05-14 12:44:19 +0900

    update posts#create linebot

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,11 +47,11 @@ class PostsController < ApplicationController
 
       # linebot
 
-      message = @post.content
-    #   {
-    #     type: 'text',
-    #     text: 'hello'
-    # }
+      message = 
+      {
+        type: 'string',
+        text: @post.content
+    }
     client = Line::Bot::Client.new { |config|
         config.channel_secret = "da5be14c010d092c6a188bf9fb79f071"
         config.channel_token = "l6MsxS40JGaFsZlSSR3br5fZ1i6Ofks3hELPEne65kqzM695FyfjS1fgVUMWB93Vt/XCa0mY9nZTdOJ7/eqYoQnFCzEEBDrV7kiILjIqqf7+1Nqj7vpxnmZO6vUgwYWhU4RzTt6hH49bykuVByprcwdB04t89/1O/w1cDnyilFU="
