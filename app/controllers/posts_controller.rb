4e754aaab6fe733372d2724bc40fa082f82646e5
[user_name] <cotaro.50tal@gmail.com>
2019-05-23 20:27:28 +0900

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -130,6 +130,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:comment,:content, :start_time)
+      params.require(:post).permit(:content, :comment, :start_time)
     end
 end
