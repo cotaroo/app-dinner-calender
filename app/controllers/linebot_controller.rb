389fb4ee53f77985ea648b111a146d94385f36f9
[user_name] <cotaro.50tal@gmail.com>
2019-05-25 19:12:48 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -41,7 +41,7 @@ class LinebotController < ApplicationController
             config.channel_secret = "da5be14c010d092c6a188bf9fb79f071"
             config.channel_token = "l6MsxS40JGaFsZlSSR3br5fZ1i6Ofks3hELPEne65kqzM695FyfjS1fgVUMWB93Vt/XCa0mY9nZTdOJ7/eqYoQnFCzEEBDrV7kiILjIqqf7+1Nqj7vpxnmZO6vUgwYWhU4RzTt6hH49bykuVByprcwdB04t89/1O/w1cDnyilFU="
         }
-        response = client.push_message("Ubb563e765d94830aa20f3a1a251de66c"), message)
+        response = client.push_message("Ubb563e765d94830aa20f3a1a251de66c", message)
         p response
 		end
 
