5064e0a801ec9f36773eaab7616acbbc96ea7154
[user_name] <cotaro.50tal@gmail.com>
2019-05-25 18:07:14 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,22 @@
 class ApplicationController < ActionController::Base
-	require 'line/bot'
+	# require 'line/bot'
 
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
 
-  before_action :validate_signature
+  # before_action :validate_signature
 
-  def validate_signature
-    body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
-  end
+  # def validate_signature
+  #   body = request.body.read
+  #   signature = request.env['HTTP_X_LINE_SIGNATURE']
+  #   unless client.validate_signature(body, signature)
+  #     error 400 do 'Bad Request' end
+  #   end
+  # end
 
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-    }
-  end
+  # def client
+  #   @client ||= Line::Bot::Client.new { |config|
+  #     config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+  #     config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+  #   }
+  # end
 end
