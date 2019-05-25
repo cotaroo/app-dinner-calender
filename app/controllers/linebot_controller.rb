b833120d8620c12745f072fdc104a902c096a524
[user_name] <cotaro.50tal@gmail.com>
2019-05-25 17:51:10 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,5 +1,5 @@
 class LinebotController < ApplicationController
-
+	require 'line/bot'
 		@@groupId = 0
 		def push
 
@@ -48,10 +48,10 @@ class LinebotController < ApplicationController
 		def recieve
 			body = request.body.read
 
-			signature = request.env['HTTP_X_LINE_SIGNATURE']
-			unless client.validate_signature(body, signature)
-				error 400 do 'Bad Request' end
-			end
+			# signature = request.env['HTTP_X_LINE_SIGNATURE']
+			# unless client.validate_signature(body, signature)
+			# 	error 400 do 'Bad Request' end
+			# end
 
 	
 			events = client.parse_events_from(body)
@@ -61,11 +61,4 @@ class LinebotController < ApplicationController
 			end
 		end
 
-		def client
-			@client ||= Line::Bot::Client.new { |config|
-				config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-				config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-			}
-		end
-		
 end
