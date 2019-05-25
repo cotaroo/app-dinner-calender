aeddb54537ce8b2d1d9ac2f6f9186ac24337ff26
[user_name] <cotaro.50tal@gmail.com>
2019-05-25 17:45:46 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -60,4 +60,12 @@ class LinebotController < ApplicationController
 				# p 'UserID: ' + userId # UserIdを確認
 			end
 		end
+
+		def client
+			@client ||= Line::Bot::Client.new { |config|
+				config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+				config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+			}
+		end
+		
 end
