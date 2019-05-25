c56f5678f03f021ada69eb8473068446625f467d
[user_name] <cotaro.50tal@gmail.com>
2019-05-25 22:16:38 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -39,10 +39,10 @@ class LinebotController < ApplicationController
 				end
 		
         client = Line::Bot::Client.new { |config|
-            config.channel_secret = "1574242480"
-            config.channel_token = "da5be14c010d092c6a188bf9fb79f071"
+            config.channel_secret = "da5be14c010d092c6a188bf9fb79f071"
+            config.channel_token = "l6MsxS40JGaFsZlSSR3br5fZ1i6Ofks3hELPEne65kqzM695FyfjS1fgVUMWB93Vt/XCa0mY9nZTdOJ7/eqYoQnFCzEEBDrV7kiILjIqqf7+1Nqj7vpxnmZO6vUgwYWhU4RzTt6hH49bykuVByprcwdB04t89/1O/w1cDnyilFU="
         }
-        response = client.push_message("Ubb563e765d94830aa20f3a1a251de66c", message)
+        response = client.push_message("Ccbf94e2e1eac61156ffd7be4aee3f1bd", message)
         p response
 		end
 		
@@ -66,8 +66,8 @@ class LinebotController < ApplicationController
 
   def client
     @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = "1574242480"
-      config.channel_token = "da5be14c010d092c6a188bf9fb79f071"
+      config.channel_secret = "da5be14c010d092c6a188bf9fb79f071"
+      config.channel_token = "l6MsxS40JGaFsZlSSR3br5fZ1i6Ofks3hELPEne65kqzM695FyfjS1fgVUMWB93Vt/XCa0mY9nZTdOJ7/eqYoQnFCzEEBDrV7kiILjIqqf7+1Nqj7vpxnmZO6vUgwYWhU4RzTt6hH49bykuVByprcwdB04t89/1O/w1cDnyilFU="
     }
   end
 
