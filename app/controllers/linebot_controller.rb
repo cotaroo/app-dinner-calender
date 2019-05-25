d1793f50e8530b941eb3a75206246a2339164576
[user_name] <cotaro.50tal@gmail.com>
2019-05-25 17:11:28 +0900

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -45,12 +45,12 @@ class LinebotController < ApplicationController
         p response
 		end
 		
-		def recieve
-			body = request.body.read
-			events = client.parse_events_from(body)
-			events.each { |event|
-				@@groupId = event['source']['groupId']  #userId取得
-				# p 'UserID: ' + userId # UserIdを確認
-			end
-		end
+		# def recieve
+		# 	body = request.body.read
+		# 	events = client.parse_events_from(body)
+		# 	events.each { |event|
+		# 		@@groupId = event['source']['groupId']  #userId取得
+		# 		# p 'UserID: ' + userId # UserIdを確認
+		# 	end
+		# end
 end
