495572bf0343461bedcec9f9281701d36031b6b6
[user_name] <cotaro.50tal@gmail.com>
2019-08-02 12:52:18 +0900

    [update]バナーメッセーの見栄えの変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,30 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery
+
+/* フラッシュ ================================ */
+$(function(){
+    $("#notice").css("display", "none");
+    
+    if($('#notice').text() != ''){
+      $("#notice").css({
+        'display' : 'block',
+        'width' : '100%',
+        'position': 'fixed',
+        'top' : '-60px',
+        'background-color' : '#FE9933',
+        'color': '#FFFFFF',
+        'z-index': '-1',
+        'border-radius': '16px',
+        'font-size': '40px',
+        'padding': '28px 0',
+        'text-align': 'center',
+      }).animate({
+        'top' : '158px',
+      },1000).delay(3000).animate({
+        'top' : '-60px',
+        'z-index': '-1'
+      },1000);
+    }
+  });
