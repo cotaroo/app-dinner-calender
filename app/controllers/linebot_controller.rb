61de69813d76869f01318f648a27654d5b4797cc
[user_name] <cotaro.50tal@gmail.com>
2019-08-08 15:33:44 +0900

    [update]期間指定投稿とLINEbotによる返信の機能完成

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -181,17 +181,17 @@ class LinebotController < ApplicationController
 				end
 		
         client = Line::Bot::Client.new { |config|
-            config.channel_secret = "68205f7a1e3339f3c020d80148a820e9"
-            config.channel_token = "RwgTi9ap0+o3zeMJNITgJ4nDbNyLZNM+t4fi2OWIpC5B7/MDJyAE3kVZA7dtKpGjPuasUU7cYvTWPivXJDxZPFdymU5isWEXVL7mFROsgUG8scfCCvBoybZwp3GRVuOqidqUr9ltcxgQnavqikFFngdB04t89/1O/w1cDnyilFU="
+            config.channel_secret = "da5be14c010d092c6a188bf9fb79f071"
+            config.channel_token = "l6MsxS40JGaFsZlSSR3br5fZ1i6Ofks3hELPEne65kqzM695FyfjS1fgVUMWB93Vt/XCa0mY9nZTdOJ7/eqYoQnFCzEEBDrV7kiILjIqqf7+1Nqj7vpxnmZO6vUgwYWhU4RzTt6hH49bykuVByprcwdB04t89/1O/w1cDnyilFU="
         }
-        response = client.push_message("Ubb563e765d94830aa20f3a1a251de66c", message)
+        response = client.push_message("Ccbf94e2e1eac61156ffd7be4aee3f1bd", message)
         p response
 		end
 		
   def client
     @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = "68205f7a1e3339f3c020d80148a820e9"
-      config.channel_token = "RwgTi9ap0+o3zeMJNITgJ4nDbNyLZNM+t4fi2OWIpC5B7/MDJyAE3kVZA7dtKpGjPuasUU7cYvTWPivXJDxZPFdymU5isWEXVL7mFROsgUG8scfCCvBoybZwp3GRVuOqidqUr9ltcxgQnavqikFFngdB04t89/1O/w1cDnyilFU="
+      config.channel_secret = "da5be14c010d092c6a188bf9fb79f071"
+      config.channel_token = "l6MsxS40JGaFsZlSSR3br5fZ1i6Ofks3hELPEne65kqzM695FyfjS1fgVUMWB93Vt/XCa0mY9nZTdOJ7/eqYoQnFCzEEBDrV7kiILjIqqf7+1Nqj7vpxnmZO6vUgwYWhU4RzTt6hH49bykuVByprcwdB04t89/1O/w1cDnyilFU="
     }
   end
 
